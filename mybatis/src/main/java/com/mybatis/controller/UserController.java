1757f87f233ecfc4a08b1e74845de56cb564b152
heyuanjing <heyuanjing@vcredit.com>
2015-07-24 17:40:13 +0800

    update

--- mybatis/src/main/java/com/mybatis/controller/UserController.java
+++ mybatis/src/main/java/com/mybatis/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.mybatis.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mybatis.common.util.LogRecord;
 import com.mybatis.mapper.IUserOperation;
-import com.mybatis.model.Article;
+import com.mybatis.model.form.UserForm;
 import com.mybatis.service.UserService;
 
 @Controller
@@ -26,11 +24,12 @@ public class UserController {
 
     @RequestMapping("/index.html")
     public String listall(HttpServletRequest request, HttpServletResponse response, Model model) {
-        List<Article> articles = userService.getArticlesByUserId(3);
-        for(Article article : articles){
-            System.out.println(article.getContent());
-        }
-        model.addAttribute("articles", articles);
+//        List<Article> articles = userService.getArticlesByUserId(3);
+//        for(Article article : articles){
+//            System.out.println(article.getContent());
+//        }
+//        model.addAttribute("articles", articles);
+        model.addAttribute("userForm",new UserForm());
         return "index";
     }
     
--- mybatis/src/main/webapp/WEB-INF/view/index.jsp
+++ mybatis/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
-    pageEncoding="utf-8"%>
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <div>
-	welcome you !
+	<h1 style="color:#566622;font-size:50px;font-familly:微软雅黑">虞琪是猪</h1>
 </div>
